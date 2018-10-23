3d0b4b0a1567f6bd52d32e1984bf7f361fa65010
pc-laptop <pc-laptop@pc-laptops-macbook-pro.local>
2018-10-23 09:26:43 -0500

    Initial Commit

---
+++ ProjectN4/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ProjectN4
+//
+//  Created by pc-laptop on 10/23/18.
+//  Copyright © 2018 Ingrid Guerrero. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
